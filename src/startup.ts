75c709a0e37ccbb20cee880199e35f97d34f34e4
st-angelo <angelostatescu@gmail.com>
2022-09-10 12:02:45 +0300

    Changed exception message

--- src/startup.ts
+++ src/startup.ts
@@ -1,6 +1,6 @@
 // Establish error handling before creating the server
 process.on('uncaughtException', (err: Error) => {
-  console.log('Uncaught Exception! Shutting down...');
+  console.log('Uncaught exception! Shutting down...');
   console.log(`${err.name}: ${err.message}`);
   process.exit(1);
 });
